65363719722f2b23272229032930798069f07388
Alexandre Figura <a.figura@syseleven.de>
2018-07-20 14:32:57 +0200

    Add tests with Pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,2 +1,10 @@
+from europython.factories import ArticleFactory
+
+
 def test_get_articles(client, db):
-    raise NotImplementedError
+    articles = ArticleFactory.create_batch(2)
+    response = client.get('/api/articles/').json
+
+    for actual, expected in zip(response, articles):
+        assert actual['id'] == expected.id
+        assert actual['title'] == expected.title
--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,6 +1,8 @@
 from pytest_bdd import given, scenario, then, when
 from pytest_bdd.parsers import parse
 
+from europython.factories import ArticleFactory
+
 
 # Scenarios
 
@@ -13,28 +15,31 @@ def test_read_article():
 
 @given(parse("I wrote {count:d} articles"))
 def articles(count, db):
-    raise NotImplementedError
+    return ArticleFactory.create_batch(count)
 
 
 # Actions
 
 @when("I access to my blog")
 def go_to_homepage(browser, server):
-    raise NotImplementedError
+    browser.visit(server.url)
 
 
 @when("I click on the first article's title")
 def go_to_first_article(browser):
-    raise NotImplementedError
+    article = browser.find_by_css('.article-list__title').first
+    article.click()
 
 
 # Assertions
 
 @then("I should be redirected to the article's page")
 def should_be_redirected(articles, browser):
-    raise NotImplementedError
+    article = articles[0]  # We are reading the first article
+    assert article.title in browser.title
 
 
 @then("I should see the article's content")
 def should_see_content(articles, browser):
-    raise NotImplementedError
+    article = articles[0]  # We are reading the first article
+    assert browser.is_text_present(article.content)
